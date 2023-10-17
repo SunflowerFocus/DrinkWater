d903270d98debec420e815e03f1bad3e3873794a
pan.feng <pan.feng@scrcnet.org>
2023-10-17 09:41:07 +0800

    delete practice

--- src/Sound.py
+++
@@ -1,5 +0,0 @@
-import winsound
-
-frequency = 440  # 频率，单位为赫兹
-duration = 6*1000  # 持续时间 单位为毫秒
-winsound.Beep(frequency, duration)
--- src/wxPython.py
+++
@@ -1,6 +0,0 @@
-import wx
-
-app = wx.App()
-frame = wx.Frame(None, title="Hello, wxPython!")
-frame.Show(True)
-app.MainLoop()
